02314221a5ef329002ce03078e425ad9ef4d3a03
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2025-01-10 18:11:31 +0530

    Refactor main layout and update Hero component with video background
    - Removed video background from main.jsx and integrated it into the Hero component for better structure.
    - Adjusted layout in Hero to include a video background, repositioned elements, and added a timer display.
    - Enhanced responsiveness by modifying image sizes and layout properties.

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 function Hero() {
   return (
     <>
-      <div className="flex flex-col items-center w-full h-screen py-8 justify-center">
-        <img src='S4DSlogo.png' className='mt-52 size-52 max-sm:size-40' />
+      <video src='background_og1.mp4' autoPlay loop muted preload='auto' className='fixed h-screen w-full object-cover -z-10'/>
+      <div className="flex flex-col items-center w-full py-8 justify-center">
+        <img src='S4DSlogo.png' className='mt-32 size-52 max-sm:size-40' />
         <p className='text-white font-bold text-2xl max-sm:text-lg my-12'>PRESENTS</p>
         <img src='Logo.png' className='max-sm:h-40 h-80' />
         <div className="bg-yellow-300 min-h-52"></div>
+      </div>
+      <div className="relative">
+        <p className='text-white font-Minecrafter text-center text-4xl'>Time Left</p>
+        <p>timer</p>
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,7 @@ import Navbar from './Components/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <video src='background_og1.mp4' autoPlay loop muted preload='auto' className='relative h-screen w-full object-cover -z-10'>
-      <div className="absolute z-10">
-        <Navbar/>
-        <Hero/>
-      </div>
-    </video>
+    <Navbar/>
+    <Hero/>
   </StrictMode>,
 )
