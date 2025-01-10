aa4ab19367597942236ff4a0eccd0deddbf345cd
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2025-01-10 15:12:01 +0530

    Refactor Hero component: move to Sections directory and remove old implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Hero from './Components/Hero.jsx'
+import Hero from './Sections/Hero.jsx'
 import Navbar from './Components/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
