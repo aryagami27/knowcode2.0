99819dc73c24aaa357587829278dc811ea7109ff
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2025-01-10 20:10:30 +0530

    Enhance About section with improved layout and content
    - Updated the About component to feature a new background color and responsive design.
    - Added descriptive text about the "Know-Code" hackathon, emphasizing creativity and collaboration.
    - Included an additional image for visual appeal and adjusted the layout for better presentation.

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full h-[60vh] bg-[#] flex justify-center items-center px-8'>
-      <div className="">
-        <img src='about section.png' className='max-h-[56vh]' />
+    <div className='relative w-full h-[60vh] bg-[#005918] flex justify-center items-center px-8'>
+      <img src='about section.png' className='sm:max-h-[56vh] h-[75%] object-cover rounded-2xl sm:rounded-none' />
+      <div className="absolute w-full flex items-center h-full top-0">
+        <div className="sm:w-[70%] w-full px-16 flex flex-col">
+          <p className='font-Minecrafter text-2xl'>ABOUT</p>
+          <p className='font-Minecraft sm:text-sm text-xs'>
+            Embark on a Techno-spheric voyage at "Know-Code", the flagship 36-hour hackathon hosted by S4DS at K J Somaiya Institute of Technology (Sion). Ignite your creativity in this domain-flexible coding marathon, where innovation knows no bounds. Unleash your coding prowess, collaborate with brilliant minds, and transform ideas into reality. Know-Code isn't just any event; it's a journey into the future of technology, a realm where every line of code is rewarded for shaping tomorrow's possibilities.
+          </p>
+        </div>
+        <div className="w-[30%] h-full sm:flex hidden justify-center">
+          <img src="Villager.png" alt="" className='h-[90%]' />
+        </div>
       </div>
     </div>
   )
