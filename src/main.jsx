bf73cef9e5ee0dd14942e3d675ed4d15109d3829
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2025-01-10 17:59:59 +0530

    Completed Footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Hero from './Sections/Hero.jsx'
-import Navbar from './Components/Navbar.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Hero from './Sections/Hero.jsx';
+import Navbar from './Components/Navbar.jsx';
+import Footer from './Components/Footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <video src='background_og1.mp4' autoPlay loop muted preload='auto' className='relative h-screen w-full object-cover -z-10'>
-      <div className="absolute z-10">
-        <Navbar/>
-        <Hero/>
+    <video
+      src='background_og1.mp4'
+      autoPlay
+      loop
+      muted
+      preload='auto'
+      className='relative h-screen w-full object-cover -z-10'
+    >
+      <div className='absolute z-10'>
+        <Navbar />
+        <Hero />
       </div>
     </video>
-  </StrictMode>,
-)
+    <Footer />
+  </StrictMode>
+);
