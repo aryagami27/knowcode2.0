374fa04d869199ec5bbc252b586e854650453e3e
rahuldhanak11 <rahuldhanak11@gmail.com>
2025-01-12 00:26:36 +0530

    margin change

--- src/Sections/Timeline.jsx
+++ src/Sections/Timeline.jsx
@@ -7,7 +7,7 @@ function Timeline() {
       className="w-full flex justify-center items-center absolute z-30 sm:top-[110vh] lg:top-[45vh] xl:top-[160vh]"
       id="timeline"
     >
-      <img src="map.png" className="sm:size-[75%] xl:size-[50%] size-[75%] mt-1" />
+      <img src="map.png" className="sm:size-[75%] xl:size-[50%] size-[75%] -mt-2" />
     </div>
   );
 }
