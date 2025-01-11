70f907792235d8cba3aca3e9e9781ca3dc83bbea
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2025-01-11 15:50:52 +0530

    Refactor Timeline component by removing GSAP animations and unnecessary SVG elements
    - Eliminated GSAP animation setup and motion path logic from the Timeline section.
    - Removed SVG and associated path elements to streamline the component.
    - Simplified the layout for improved readability and maintainability.

--- src/Sections/Prizepool.jsx
+++ src/Sections/Prizepool.jsx
@@ -66,7 +66,7 @@ function Prizepool() {
       <div className="bg-passage bg-center bg-no-repeat bg-cover flex justify-center py-8 px-4">
         <div className="w-full bg-signbkg rounded-2xl py-4 px-2 text-black max-w-[1200px] h-full gap-4 flex flex-col justify-center">
           <p className="text-4xl font-Minecrafter text-center ">Guideline</p>
-          <div className="font-Minecraft max-sm:text-xs lg:text-lg text-justify px-4" style={{textShadow:"0 1px black"}}>
+          <div className="font-medium max-sm:text-xs lg:text-lg text-justify px-4">
             <p>
               1) Participants are required to remain within the college premises
               during the Hackathon, seeking permission from volunteers if there's
