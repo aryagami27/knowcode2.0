09e60ea84c2a1b0a77c76f29c44f64f21e44ae7e
rahuldhanak11 <rahuldhanak11@gmail.com>
2025-01-11 18:17:16 +0530

    Images added

--- src/Sections/Theme.jsx
+++ src/Sections/Theme.jsx
@@ -72,28 +72,61 @@ function Theme() {
         />
       </div>
       <div className="h-[50vh] w-full flex justify-center bg-leave bg-repeat pt-10">
-        <div className="flex flex-wrap justify-center space-x-2">
-        <div className="sm:size-28 size-20 bg-hotbar bg-cover"></div>
-          <div className="sm:size-28 size-20  bg-hotbar bg-cover">
-
+        <div className="flex flex-wrap justify-center sm:space-x-5 space-x-2  text-xl">
+        <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="blockchain1.png" alt="blockchain" className='size-18'/>
           </div>
-          <div className="sm:size-28 size-20 bg-hotbar bg-cover">
-            
+          <p className='text-white mt-1'>AI/ML</p>
           </div>
-          <div className="sm:size-28 size-20 bg-hotbar bg-cover">
 
+          <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="Painting.png" alt="frontend" className='size-20 mb-1 mr-1'/>
           </div>
-          <div className="sm:size-28 size-20 bg-hotbar bg-cover">
-
+          <p className='text-white mt-1'>Frontend </p>
           </div>
-          <div className="sm:size-28 size-20 bg-hotbar bg-cover">
 
+          <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="cybersec1.png" alt="cybersecurity" className='size-24 mt-2'/>
           </div>
-          <div className="sm:size-28 size-20 bg-hotbar bg-cover">
-
+          <p className='text-white mt-1'>CyberSecurity</p>
           </div>
-          <div className="sm:size-28 size-20 bg-hotbar bg-cover">
 
+          <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="cloudnative1.png" alt="blockchain" className='size-28'/>
+          </div>
+          <p className='text-white mt-1'>Cloud Native</p>
+          </div>
+
+          <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="datascience.png" alt="frontend" className='size-20 mt-2 ml-1'/>
+          </div>
+          <p className='text-white mt-1'>Data Science</p>
+          </div>
+
+          <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="web3.png" alt="frontend" className='size-16'/>
+          </div>
+          <p className='text-white mt-1'>WEB3</p>
+          </div>
+
+          <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="bitcoin.png" alt="blockchain" className='size-20'/>
+          </div>
+          <p className='text-white mt-1'>BlockChain</p>
+          </div>
+
+          <div className='flex flex-col items-center'>
+          <div className="sm:size-28 size-20  bg-hotbar bg-cover flex justify-center items-center">
+            <img src="Iot.png" alt="frontend" className='size-16'/>
+          </div>
+          <p className='text-white mt-1'>IOT</p>
           </div>
         </div>
       </div>
