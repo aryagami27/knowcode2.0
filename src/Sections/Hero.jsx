ad2d458dac5755222fd72a38d085c900cdcd9981
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2025-01-12 16:20:45 +0530

    Comment out clock display in Hero section for future implementation

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -22,10 +22,10 @@ function Hero() {
         <img src='Logo.png' id='logo' className='max-sm:h-40 h-80 translate-y-40 opacity-0' />
         <div className="bg-yellow-300 min-h-52"></div>
       </div>
-      <div className="text-[#C2C2C2] h-screen flex justify-center items-center flex-col" id='clock'>
+      {/* <div className="text-[#C2C2C2] h-screen flex justify-center items-center flex-col" id='clock'>
         <p className=' font-Minecrafter text-center text-4xl' style={{textShadow: "0px 8px #000000",WebkitTextStroke: "2px black"}}>Time Left</p>
         <p className='font-Minecrafteralt sm:text-8xl text-6xl text-center pb-[50vh]' style={{textShadow: "0px 12px #000000",WebkitTextStroke: "2px black"}}>1D:11H:11M</p>
-      </div>
+      </div> */}
     </div>
   )
 }
