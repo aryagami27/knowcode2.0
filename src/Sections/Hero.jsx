7262af2c8392328d58aaed57560588993750050e
rahuldhanak11 <rahuldhanak11@gmail.com>
2025-01-12 18:40:24 +0530

    timer size change

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -61,7 +61,7 @@ function Hero() {
         id="clock"
       >
         <p
-          className="font-Minecrafter text-center text-4xl"
+          className="font-Minecrafter text-center sm:text-4xl text-2xl"
           style={{
             textShadow: "0px 8px #000000",
             WebkitTextStroke: "2px black",
@@ -70,7 +70,7 @@ function Hero() {
           Time Left
         </p>
         <p
-          className="font-Minecrafteralt sm:text-8xl text-6xl text-center pb-[50vh]"
+          className="font-Minecrafteralt sm:text-8xl text-4xl text-center pb-[50vh]"
           style={{
             textShadow: "0px 12px #000000",
             WebkitTextStroke: "2px black",
